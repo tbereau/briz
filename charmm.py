b659de861bf16ffb03b0c0c6b0282f369906a1a8
Tristan Bereau <tristan.bereau@unibas.ch>
2013-03-27 18:37:05 +0100

    more work on ssh and charmm

--- charmm.py
+++ charmm.py
@@ -8,8 +8,11 @@ class RunCharmmRemotely(ssh.RunCommandRemotely):
 		ssh.RunCommandRemotely.__init__(self, server=server, subdir=subdir)
 		self.charmmInp = ""
 		self.charmmOut = ""
+		self.subjobs   = []
 
-	def generateCharmmJob(self):
+	def generateCharmmJob(self, inpFile, outFile):
+		self.charmmInp = inpFile
+		self.charmmOut = outFile
 		return \
 '''#!/bin/bash
 #$ -m a
@@ -19,26 +22,60 @@ input=%s
 output=%s
 tempdir=%s/$JOB_ID
 mkdir -p $tempdir
-oridir=${SGE_O_WORKDIR}
+oridir=%s
 
 cp -r $oridir/* $tempdir/
+cd $tempdir
 mpirun=%s
 charmm=%s
 if [  "$NSLOTS" -gt 1 ]; then
-  $mpirun -v -np $NSLOTS $charmm < $tempdir/$input > $tempdir/$output
+  $mpirun -v -np $NSLOTS $charmm < $input > $output
 else
-    $charmm < $tempdir/$input > $tempdir/$output
+    $charmm < $input > $output
 fi
 
-mv $tempdir/* $oridir/
+cd $oridir
+mv $tempdir/* $oridir
 rmdir $tempdir
 ''' % (self.config.get('misc','email'), self.charmmInp, self.charmmOut,
-	self.config.get(server, 'scratchdir'), self.config.get(server,'mpirun'),
-	self.config.get(server, 'charmm'))
+	self.config.get(self.server, 'scratchdir'), self.remdir,
+	self.config.get(self.server,'mpirun'),
+	self.config.get(self.server, 'charmm'))
 
+	def remoteSimulationTerminatedNormally(self, myFile):
+		# Analyze remote simulation output log. Return True if it's reached Normal
+		# Termination; False otherwise.
+		baseFile = myFile
+		if baseFile.find("/") != -1:
+			baseFile = myFile[myFile.rfind("/")+1:]
+		remFile = self.remdir + "/" + baseFile
+		try:
+			f = self.sftp.open(remFile,'r')
+			s = f.readlines()
+			f.close()
+		except IOError, e:
+			print "I/O Error: {0}".format(e.strerror)
+			return False
+		if len(s) < 11:
+			return False
+		for i in range(len(s)-10,len(s)):
+			if "NORMAL TERMINATION BY NORMAL STOP" in s[i]:
+				return True
+		return False
+
+	def consistentAndGet(self, myFile):
+		# Analyze remote simulation and get file. Return True if it was
+		# successful, False otherwise.
+		if self.remoteFileExists(myFile) is False:
+			return False
+		status = self.remoteSimulationTerminatedNormally(myFile)
+		if status == False:
+			return False
+		self.getFile(myFile)
+		return True
 
 if __name__ == "__main__":
 	# Testing with verdi
 	server = 'verdi'
 	rmtChm = RunCharmmRemotely(server, subdir='dir000000')
-	print rmtChm.generateCharmmJob()
+	print rmtChm.generateCharmmJob(inpFile='sim.inp', outFile='sim.out')
--- ssh.py
+++ ssh.py
@@ -11,30 +11,40 @@ class RunCommandRemotely:
 		hostsFile = __file__[:__file__.rfind("/")] + "/hosts.ini"
 		self.config.read(hostsFile)	
 
-		self.locdir    = os.getcwd()
-		self.server    = server
-		self.hostname  = self.config.get(server, 'hostname')
-		self.username  = self.config.get(server, 'user')
-		self.workdir   = self.config.get(server, 'workdir')
-		self.subdir    = subdir
-		self.remdir    = ''
-		self.pkeyfile  = os.path.expanduser('~/.ssh/id_rsa')
-		self.mykey     = paramiko.RSAKey.from_private_key_file(
+		self.locdir      = os.getcwd()
+		self.server      = server
+		self.hostname    = self.config.get(server, 'hostname')
+		self.username    = self.config.get(server, 'user')
+		self.workdir     = self.config.get(server, 'workdir')
+		self.subdir      = subdir
+		self.remdir      = ''
+		self.pkeyfile    = os.path.expanduser('~/.ssh/id_rsa')
+		self.mykey       = paramiko.RSAKey.from_private_key_file(
 			self.pkeyfile)
-		self.trnsprt   = paramiko.Transport((self.hostname, 22))
+		self.trnsprt     = paramiko.Transport((self.hostname, 22))
 		self.trnsprt.connect(username=self.username, pkey=self.mykey)
-		self.sftp      = paramiko.SFTPClient.from_transport(self.trnsprt)
-		self.ssh       = paramiko.SSHClient()
-		self.stdin     = ''
-		self.stdout    = ''
-		self.stderr    = ''
-		self.maxtrials = 20
+		self.sftp        = paramiko.SFTPClient.from_transport(self.trnsprt)
+		self.ssh         = paramiko.SSHClient()
+		self.stdin       = ''
+		self.stdout      = ''
+		self.stderr      = ''
+		self.maxtrials   = 20
+		self.numprocflag = self.config.get(server, 'numprocflag')
+		self.queuespec   = self.config.get(server, 'queuespec' )
+		self.queuespecn  = self.config.get(server, 'queuespecn')
+		self.quejobidcol = self.config.get(server, 'quejobidcol')
 
 		self.ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 
 		# Connect to remote server, choose directory, and create working directory
 		self.chooseDir()
 
+	def die(self):
+		for line in self.stdout.readlines():
+			print line
+		for line in self.stderr.readlines():
+			print line
+		exit(1)
 
 	def connectSSH(self):
 		if hasattr(self.ssh,"is_active") is False:
@@ -61,6 +71,9 @@ class RunCommandRemotely:
 				if sout.readlines()[0].strip() == "0":
 					freeDir = True
 				dirCounter += 1
+				if dirCounter > 999999:
+					print "No free directory in",self.workdir
+					exit(1)
 			self.subdir   = dirName
 		else:
 			# If folder already exists, move it to .bak
@@ -69,9 +82,33 @@ class RunCommandRemotely:
 			self.execCmd( "mv " + self.workdir + "/" + self.subdir 
 				+ " " + self.workdir + "/" + self.subdir + ".bak")
 		self.remdir   = self.workdir + "/" + self.subdir
-		print self.remdir
 		self.execCmd("mkdir -p " + self.remdir)
 		self.disconnectSSH()
+
+	def remoteFileExists(self, myFile):
+		baseFile = myFile
+		if baseFile.find("/") != -1:
+			baseFile = myFile[myFile.rfind("/")+1:]
+		destFile = self.remdir + "/" + baseFile
+		trials = 0
+		statSuccess = False
+		while trials < self.maxtrials and statSuccess == False:
+			try:
+				self.sftp.stat(destFile)
+			except:
+				trials += 1
+				continue
+			break
+		if trials == self.maxtrials:
+			return False
+		return True
+
+	def localFileExists(self, myFile):
+		baseFile = myFile
+		if baseFile.find("/") != -1:
+			baseFile = myFile[myFile.rfind("/")+1:]
+		oriFile = os.path.abspath(myFile)
+		return os.path.exists(oriFile)
 
 	def putFile(self, myFile):
 		baseFile = myFile
@@ -91,7 +128,8 @@ class RunCommandRemotely:
 		if trials == self.maxtrials:
 			print "Error. Can't copy", myFile
 			exit(1)
-		print "copied",baseFile,"to the remote server"
+		# print "copied",baseFile,"to the remote server"
+		return
 
 	def getFile(self, myFile):
 		baseFile = myFile
@@ -100,7 +138,7 @@ class RunCommandRemotely:
 		remFile = self.remdir + "/" + baseFile
 		trials = 0
 		getSuccess = False
-		while trials < self.maxtrials and putSuccess == False:
+		while trials < self.maxtrials and getSuccess == False:
 			try:
 				self.sftp.get(remFile, self.locdir + "/" + baseFile)
 			except:
@@ -110,12 +148,67 @@ class RunCommandRemotely:
 		if trials == self.maxtrials:
 			print "Error. Can't copy", myFile
 			exit(1)
-		print "copied",baseFile,"from the remote server"
+		# print "copied",baseFile,"from the remote server"
+		return
+
+	def delFile(self, myFile):
+		baseFile = myFile
+		if baseFile.find("/") != -1:
+			baseFile = myFile[myFile.rfind("/")+1:]
+		remFile = self.remdir + "/" + baseFile
+		trials = 0
+		delSuccess = False
+		while trials < self.maxtrials and delSuccess == False:
+			try:
+				self.sftp.remove(remFile)
+			except:
+				trials += 1
+				continue
+			break
+		if trials == self.maxtrials:
+			return False
+		return True			
 
 	def execCmd(self, cmd):
 		self.stdin, self.stdout, self.stderr = self.ssh.exec_command(cmd)
 		# The following is a blocking command.
 		return self.stdout.channel.recv_exit_status()
+
+	def submitJob(self, jobName, numProc, inpCmd, dependID=0):
+		# Submit job to queueing system. Return job ID.
+		queuesub   = ""
+		numprocsub = ""
+		depend     = ""
+		if int(numProc) > 1:
+			numprocsub = self.numprocflag + " " + numProc
+			if int(numProc) >= self.queuespecn:
+				queuesub = "-q " + self.queuespec
+		if dependID > 0:
+			depend = "-hold_jid " + str(dependID)
+		status = self.execCmd("qsub -S /bin/sh -cwd -N " + jobName \
+			+ " -j y " + numprocsub + " " + queuesub + " " + depend \
+			+ " -o " + self.remdir + "/" + jobName + ".log " + inpCmd)
+		if status != 0:
+			print "Error: qsub submission failed. Error code", status
+			self.die()
+		# Return job ID
+		self.execCmd("qstat | grep " + self.username + " | grep " \
+			+ jobName[:10] + " | awk '{print $" + self.quejobidcol + "}'")
+		return int(self.stdout.readlines()[0].split()[0]) 
+
+	def jobIsRunning(self, jobName):
+		if jobName == "":
+			return False
+		self.execCmd("qstat | grep " + self.username + " | grep " \
+			+ str(jobName) + " | wc -l")
+		numLines = int(len(self.stdout.readlines()))
+		if numLines == 1:
+			return True
+		elif numLines == 0:
+			return False
+		else:
+			print "Error. grep of " + jobName + " returned too many results."
+			self.die()
 
 	def getStdin(self):
 		return self.stdin.readlines()
