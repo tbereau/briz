66be3e23f9eea20cf6e344173fc2eedc2042b450
Tristan Bereau <tristan.bereau@unibas.ch>
2013-04-11 13:57:53 +0200

    find files locally

--- misc.py
+++ misc.py
@@ -1,6 +1,7 @@
 '''Misc routines'''
 
 import os
+import subprocess
 
 def readInFromFile(fileName):
   '''Read in fileName and output string containing whole content.'''
@@ -46,4 +47,15 @@ def assertProgramIsReachable(progName):
   if os.system("type " + progName + " > /dev/null") != 0:
       print "# Error. Can't find " + progName + " on local machine."
       exit(1)
+  return 
+
+def findFilesInLocalDir(subString):
+  # Return all local files that contain subString
+  locFiles = subprocess.check_output(
+    "find . -depth ! -path \"*/\.*\" -name \"*" + subString 
+      + "*\" ", shell=True).split("\n")
+  return locFiles
+
+def removeFile(myFile):
+  subprocess.check_output("rm -f " + myFile, shell=True)
   return 
